d0780ccbdb3762aab54ad7f8261f2216f7959e1f
thebadsektor <thebadsektor@gmail.com>
2023-06-11 22:38:10 +0800

    setup toml

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 from langchain.llms import OpenAI
 import pandas as pd
 from langchain.agents import create_csv_agent
+import streamlit as st
 
-api_key = os.environ.get("OPENAI_API_KEY")
+# api_key = os.environ.get("OPENAI_API_KEY")
+api_key = os.environ.get(st.secrets["OPENAI_API_KEY"])
 
 if api_key:
     print("OpenAI API key is good.")
@@ -17,6 +19,6 @@ print(df[0:10])
 
 csv_path = 'data/knowledge-base.csv'
 
-csv_agent = create_csv_agent(OpenAI(model_kwargs={"api_key": api_key, "temperature": 0}), csv_path, verbose=True)
+csv_agent = create_csv_agent(OpenAI(temperature=0), csv_path, verbose=True)
 output = csv_agent.run("Look for similar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?")
 print(output)
