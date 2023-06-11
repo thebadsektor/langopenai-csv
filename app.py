aa067a80d4c696045d30bfa31ed93e788cf12451
thebadsektor <thebadsektor@gmail.com>
2023-06-11 22:17:37 +0800

    using env for api key

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
-os.environ["OPENAI_API_KEY"] = "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"
-
 from langchain.llms import OpenAI
 import pandas as pd
 from langchain.agents import create_csv_agent
 
-df = pd.read_csv('data/knowledge-base.csv')
-print(df[0:10])
+api_key = os.environ.get("OPENAI_API_KEY")
 
-csv_path = 'data/knowledge-base.csv'
+if api_key:
+    # API key is set as an environment variable
+    df = pd.read_csv('data/knowledge-base.csv')
+    print(df[0:10])
 
-csv_agent = create_csv_agent(OpenAI(temperature=0), csv_path, verbose=True)
-output = csv_agent.run("Look for similiar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?")
-print(output)
+    csv_path = 'data/knowledge-base.csv'
+
+    csv_agent = create_csv_agent(OpenAI(api_key=api_key, temperature=0), csv_path, verbose=True)
+    output = csv_agent.run("Look for similar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?")
+    print(output)
+else:
+    # API key is not set
+    print("OpenAI API key is not provided as an environment variable.")
